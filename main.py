e20b2ef146ae046a0d3616d47cf0a1e5bfd81d18
mayapayaa <m.vantoto@gmail.com>
2025-01-17 04:22:47 +0000

    Collect user's age with error handling

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ print("It's designed to help gather valuable feedback and insights.")
 print("Whether you need to improve products, understand customer satisfaction, or collect opinions, Survey Bot is here to assist.")
 
 name = input("What is your name? ")
-age = input("Hello " + name + ", how old are you? ")
+
+try:
+    age = int(input("How old are you? "))
+except ValueError:
+    print("Please enter a valid age.")
+    age = int(input("How old are you? "))
+    
 question = input("How can it help you today? ")
