887b5b12532927381196f50d4cc0c072d41692f5
mayapayaa <m.vantoto@gmail.com>
2025-01-17 03:33:47 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome! This is the Survey Bot.")
+print("It's designed to help gather valuable feedback and insights.")
+print("Whether you need to improve products, understand customer satisfaction, or collect opinions, Survey Bot is here to assist.")
+
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+question = input("How can it help you today? ")
