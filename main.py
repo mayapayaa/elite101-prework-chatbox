f2ac5841cc13e79d6baa06625edd22f5bc2a236b
mayapayaa <m.vantoto@gmail.com>
2025-01-17 04:25:44 +0000

    Display menu options for survey feedback

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ try:
 except ValueError:
     print("Please enter a valid age.")
     age = int(input("How old are you? "))
-    
+
 question = input("How can it help you today? ")
+
+print("\nHere are some options to continue:")
+print("1. Take a product feedback survey")
+print("2. Provide customer satisfaction feedback")
+print("3. Exit")
